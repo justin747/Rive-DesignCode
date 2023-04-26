5a8b66007a0750b937e225bace6d359d94a723b0
justin747 <justin747@gmail.com>
2023-04-25 21:50:58 -0700

    Initial Commit

---
+++ Rive DesignCode/Rive_DesignCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rive_DesignCodeApp.swift
+//  Rive DesignCode
+//
+//  Created by Justin on 4/25/23.
+//
+
+import SwiftUI
+
+@main
+struct Rive_DesignCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
